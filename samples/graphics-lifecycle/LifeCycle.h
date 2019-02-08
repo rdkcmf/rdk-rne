27007d3f01807e097260f26f643e9a9079945e59
lakshmipriyap <Lakshmipriya_Purushan@comcast.com>
2019-02-08 17:09:17 +0530

    RDKMDOC-60: RNE Doxygen Documentation
    Reason for change: Added Doxygen Annotation in RNE Source code.
    Test Procedure: Verify build is successful
    Risks: Low
    Signed-off-by: Lakshmipriya <lakshmipriyap@tataelxsi.co.in>
    
    Change-Id: I6652b5e4421322e41c8a49324caab35e969300ca

--- samples/graphics-lifecycle/LifeCycle.h
+++ samples/graphics-lifecycle/LifeCycle.h
@@ -20,10 +20,63 @@
 #ifndef _GRAPHICS_LIFECYCLE_H_
 #define _GRAPHICS_LIFECYCLE_H_
 
+/**
+ * @defgroup FIREBOLT Firebolt
+ *
+ * RDK Firebolt SDK (previously called RNE) is intended to provide a development environment for applications targeted to run on RDK.
+ * Firebolt SDK can be used by application developers to develop HTML5, Spark & Native applications for RDK.
+ * The SDK comes with an RDK image which can be loaded on the target platform on which applications can be developed and executed.
+ *
+ * @defgroup Graphics Sample Graphics Sample
+ * 
+ * rne-triangle sample app demonstrates a simple graphics app that uses opengl es and wayland to render a spinning triangle to the screen * The following key features are demostrated:
+ * - How to use wayland and OpenGL ES to render graphics
+ * - How to get keyboard/remote input
+ * - How to get screen resolution
+ * @ingroup FIREBOLT
+ * 
+ * @defgroup Graphics-lifecycle Graphics-lifecycle
+ * 
+ * Graphics-lifecycle sample app extends the rne-triangle sample app to support rt and suspend/resume.
+ * The following key features are demonstrated:
+ * - How to use wayland and OpenGL ES to render graphics
+ * - How to get keyboard/remote input
+ * - How to get screen resolution
+ * - How to setup and register an object with rt
+ * - How to handle suspend/resume 
+ * @ingroup FIREBOLT
+ *
+ * @defgroup MSE Player Sample MSE Player Sample
+ *
+ * The MSE (Media Source Extensions) player sample app demos how to put everything together for a more real world example.
+ * The app will show how to build a gstreamer pipeline that can be fed raw H264 and AAC frames asynchronously. 
+ * The sample content contains three separate fragments of the same video to show how to simulate a seek by flushing the video pipeline
+ * and providing new samples at different period in time.
+ * The app also uses the essos library which simplifies setting up wayland for graphics and keyboard/remote input.
+ * The following key features are demonstrated:
+ * - How to setup and use a more complicated gstreamer pipeline with a custom source
+ * - How to setup and register an object with rt
+ * - How to handle suspend/resume
+ * @ingroup FIREBOLT
+ *
+ * @defgroup Player Sample Player Sample
+ * 
+ * rne-player sample app shows how to build and use a simple gstreamer pipeline that uses westerosink.
+ * It will load a movie from a URL, buffer it, and play it back. The following key features are demonstrated:
+ * - How to build a simple gstreamer pipeline with westerossink
+ * - How to get screen resolution 
+ * @ingroup FIREBOLT
+ */
+
 #include <rtRemote.h>
 #include <rtError.h>
 
 #include "RtUtils.h"
+
+/**
+ * @addtogroup Graphics-lifecycle
+ * @{
+ */
 
 class GraphicsLifeCycle : public rtObject {
 public:
@@ -33,15 +86,49 @@ public:
 
     GraphicsLifeCycle();
     virtual ~GraphicsLifeCycle();
+
+	/**
+	 *  @brief This API is called by the app manager to suspend the currently running application.
+     *
+     *  Apps when suspended should free all graphics and AV resources.
+     *  Uses  minimal CPU and RAM while running in the background.
+     *  The app manager invokes suspend call from the app through rt.
+	 *
+	 *  @return Returns RT_OK on success, appropriate error code otherwise.
+	 */
     rtError suspend();
+
+	/**
+	 *  @brief This API is used to resume the application suspended by the app manager.
+	 *
+	 *  @return Returns RT_OK on success, appropriate error code otherwise.
+	 */
     rtError resume();
 
+    /**
+	 *  @brief This initializes the rt object, sets the event listeners and also set the remote ready flag.
+	 *
+	 *  @param[in]  rtUtils   rt object.
+	 *
+	 *  @return Returns RMF_SUCCESS on success, appropriate error code otherwise.
+	 */
     void setRtUtils(RtUtils* rtUtils);
 
     struct Callbacks {
        void (*onResume)(const char* p);
        void (*onSuspend)();
     };
+
+	/**
+	 *  @brief This API sets the callback functions.
+     *
+     *  Apps should support a suspend and resume option if needed 
+     *  so they can start quickly and stay running in the background. 
+	 *
+	 *  @param[in] cb   Callback functions to set.
+	 *
+	 *  @return Returns RMF_SUCCESS on success, appropiate error code otherwise.
+	 */
     void setCallbacks(const Callbacks &cb);
 private:
     RtUtils* mRtUtils;
@@ -49,3 +136,7 @@ private:
 };
 
 #endif
+/**
+ * @}
+ */
+
