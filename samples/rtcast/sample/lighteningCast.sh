f3d18630bec0e71afed38ba28d656a8f24480c7d
jkuria002c <Josekutty_Kuriakose@cable.comcast.com>
2019-04-30 20:02:40 +0000

    DELIA-35140 : Remove unused rtcast files
    Reason for change: No need for the script of pxscene app to distribute
    Test Procedure: Make sure the app builds fine.
    Risks: Low
    
    Change-Id: I6340315709fc784b731ebc2e9337695167ea3576
    Signed-off-by: jkuria002c <Josekutty_Kuriakose@cable.comcast.com>

--- samples/rtcast/sample/lighteningCast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export XDG_RUNTIME_DIR=/run/user/
-export LD_PRELOAD=/usr/lib/libwayland-client.so.0:/usr/lib/libwayland-egl.so.0
-#export WAYLAND_DISPLAY=lcast
-export WAYLAND_APPS_CONFIG=/home/root/waylandregistryreceiver.conf
-
-/home/root/Spark /usr/share/xdial/lighteningCast.js
